1111957f30356f01457a4b8fbfac52b15c917ba5
lvyiyang <lvyiyang@didichuxing.com>
2020-11-16 16:16:15 +0800

    fix ut of ProcessInstanceMapper

--- turbo-engine/src/test/java/com/xiaoju/uemc/turbo/engine/dao/mapper/ProcessInstanceMapperTest.java
+++ turbo-engine/src/test/java/com/xiaoju/uemc/turbo/engine/dao/mapper/ProcessInstanceMapperTest.java
@@ -4,6 +4,7 @@ import com.xiaoju.uemc.turbo.engine.common.FlowInstanceStatus;
 import com.xiaoju.uemc.turbo.engine.entity.FlowInstancePO;
 import com.xiaoju.uemc.turbo.engine.runner.BaseTest;
 import com.xiaoju.uemc.turbo.engine.util.EntityBuilder;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +32,7 @@ public class ProcessInstanceMapperTest extends BaseTest {
         processInstanceMapper.insert(flowInstancePO);
         String flowInstanceId = flowInstancePO.getFlowInstanceId();
         flowInstancePO = processInstanceMapper.selectByFlowInstanceId(flowInstancePO.getFlowInstanceId());
-        Assert.assertTrue(flowInstancePO.getFlowInstanceId() == flowInstanceId);
+        Assert.assertTrue(StringUtils.equals(flowInstancePO.getFlowInstanceId(),flowInstanceId));
         flowInstancePO.setStatus(FlowInstanceStatus.TERMINATED);
         flowInstancePO.setModifyTime(new Date());
         processInstanceMapper.updateStatus(flowInstancePO);
