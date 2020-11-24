a98dd9e548dfa97ac694c697b6062d89938ee1c6
zuokai <zuokai@didiglobal.com>
2020-11-24 15:30:39 +0800

    add comment

--- turbo-engine/src/test/java/com/xiaoju/uemc/turbo/engine/error/Error4Test.java
+++ turbo-engine/src/test/java/com/xiaoju/uemc/turbo/engine/error/Error4Test.java
@@ -3,9 +3,7 @@ package com.xiaoju.uemc.turbo.engine.error;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.xiaoju.uemc.turbo.engine.bo.NodeInstanceBO;
-import com.xiaoju.uemc.turbo.engine.common.ErrorEnum;
-import com.xiaoju.uemc.turbo.engine.common.NodeInstanceStatus;
-import com.xiaoju.uemc.turbo.engine.common.RuntimeContext;
+import com.xiaoju.uemc.turbo.engine.common.*;
 import com.xiaoju.uemc.turbo.engine.dto.CommitTaskDTO;
 import com.xiaoju.uemc.turbo.engine.dto.RecallTaskDTO;
 import com.xiaoju.uemc.turbo.engine.dto.StartProcessDTO;
@@ -22,13 +20,17 @@ import com.xiaoju.uemc.turbo.engine.param.RecallTaskParam;
 import com.xiaoju.uemc.turbo.engine.param.StartProcessParam;
 import com.xiaoju.uemc.turbo.engine.processor.RuntimeProcessor;
 import com.xiaoju.uemc.turbo.engine.runner.BaseTest;
+import com.xiaoju.uemc.turbo.engine.util.FlowModelUtil;
+import com.xiaoju.uemc.turbo.engine.util.GroovyUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 错误码覆盖单元测试类之4000~4999 流程执行错误
@@ -250,8 +252,77 @@ public class Error4Test extends BaseTest {
     }
 
     // GET_INSTANCE_DATA_FAILED(4011, "获取不到实例数据信息")
+    @Test
+    public void error_4011() {
+        // depend on db data
+    }
+
     // GET_HOOK_CONFIG_FAILED(4012, "获取不到hook配置")
+    @Test
+    public void error_4012() {
+        // first, config hookInfoIds in ExclusiveGateway node
+        // second, set optimus_prime.hook.url empty
+        StartProcessDTO startProcessDTO = startProcess();
+        CommitTaskParam commitTaskParam = new CommitTaskParam();
+        commitTaskParam.setFlowInstanceId(startProcessDTO.getFlowInstanceId());
+        commitTaskParam.setTaskInstanceId(startProcessDTO.getActiveTaskInstance().getNodeInstanceId());
+        List<InstanceData> variables = new ArrayList<>();
+        variables.add(new InstanceData("danxuankuang_ytgyk", "int", 0));
+        commitTaskParam.setVariables(variables);
+        try {
+            // user task -> exclusive gateway node -> user task
+            CommitTaskDTO commitTaskDTO = runtimeProcessor.commit(commitTaskParam);
+            Assert.assertTrue(commitTaskDTO.getErrCode() == ErrorEnum.GET_HOOK_CONFIG_FAILED.getErrNo());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     // GET_OUTGOING_FAILED(4013, "找不到下一个待执行节点")
+    @Test
+    public void error_4013() {
+        try {
+            FlowElement currentFlowElement = new FlowElement();
+            currentFlowElement.setKey("ExclusiveGateway_0yq2l0s");
+            currentFlowElement.setType(FlowElementType.EXCLUSIVE_GATEWAY);
+            currentFlowElement.setIncoming(Lists.newArrayList());
+            currentFlowElement.setOutgoing(Lists.newArrayList("SequenceFlow_15rfdft", "SequenceFlow_1lc9xoo"));
+
+            FlowElement SequenceFlow_15rfdft = new FlowElement();
+            SequenceFlow_15rfdft.setKey("SequenceFlow_15rfdft");
+            SequenceFlow_15rfdft.setType(FlowElementType.SEQUENCE_FLOW);
+            SequenceFlow_15rfdft.setIncoming(Lists.newArrayList("ExclusiveGateway_0yq2l0s"));
+            SequenceFlow_15rfdft.setOutgoing(Lists.newArrayList());
+            Map<String, Object> properties = new HashMap<>();
+            properties.put(Constants.ELEMENT_PROPERTIES.CONDITION, "orderId.equals(\"123\")");
+            SequenceFlow_15rfdft.setProperties(properties);
+
+            FlowElement SequenceFlow_1lc9xoo = new FlowElement();
+            SequenceFlow_1lc9xoo.setKey("SequenceFlow_1lc9xoo");
+            SequenceFlow_1lc9xoo.setType(FlowElementType.SEQUENCE_FLOW);
+            SequenceFlow_1lc9xoo.setIncoming(Lists.newArrayList("ExclusiveGateway_0yq2l0s"));
+            SequenceFlow_1lc9xoo.setOutgoing(Lists.newArrayList());
+            Map<String, Object> _properties = new HashMap<>();
+            _properties.put(Constants.ELEMENT_PROPERTIES.CONDITION, "orderId.equals(\"456\")");
+            SequenceFlow_1lc9xoo.setProperties(_properties);
+
+            Map<String, FlowElement> flowElementMap = new HashMap<>();
+            flowElementMap.put("ExclusiveGateway_0yq2l0s", currentFlowElement);
+            flowElementMap.put("SequenceFlow_15rfdft", SequenceFlow_15rfdft);
+            flowElementMap.put("SequenceFlow_1lc9xoo", SequenceFlow_1lc9xoo);
+
+            Map<String, InstanceData> instanceDataMap = new HashMap<>();
+            instanceDataMap.put("orderId", new InstanceData("orderId", "string", "123456"));
+
+            FlowModelUtil.calculateNextNode(currentFlowElement, flowElementMap, instanceDataMap);
+        } catch (ProcessException e) {
+            e.printStackTrace();
+            Assert.assertTrue(e.getErrNo() == ErrorEnum.GET_OUTGOING_FAILED.getErrNo());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     // UNSUPPORTED_ELEMENT_TYPE(4014, "不支持的节点操作")
     @Test
     public void error_4014() {
@@ -268,6 +339,18 @@ public class Error4Test extends BaseTest {
     }
 
     // MISSING_DATA(4015, "表达式运算缺少数据")
+    @Test
+    public void error_4015() {
+        try {
+            GroovyUtil.calculate("orderId.equals(\"123\")", new HashMap<>());
+        } catch (ProcessException e) {
+            e.printStackTrace();
+            Assert.assertTrue(e.getErrNo() == ErrorEnum.MISSING_DATA.getErrNo());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     // SAVE_FLOW_INSTANCE_FAILED(4016, "保存流程实例失败")
     @Test
     public void error_4016() {
@@ -282,8 +365,47 @@ public class Error4Test extends BaseTest {
 
     // GROOVY_CALCULATE_FAILED(4018, "表达式执行失败")
     @Test
-    public void error_4() {
+    public void error_4018() {
+        try {
+            FlowElement currentFlowElement = new FlowElement();
+            currentFlowElement.setKey("ExclusiveGateway_0yq2l0s");
+            currentFlowElement.setType(FlowElementType.EXCLUSIVE_GATEWAY);
+            currentFlowElement.setIncoming(Lists.newArrayList());
+            currentFlowElement.setOutgoing(Lists.newArrayList("SequenceFlow_15rfdft", "SequenceFlow_1lc9xoo"));
 
+            FlowElement SequenceFlow_15rfdft = new FlowElement();
+            SequenceFlow_15rfdft.setKey("SequenceFlow_15rfdft");
+            SequenceFlow_15rfdft.setType(FlowElementType.SEQUENCE_FLOW);
+            SequenceFlow_15rfdft.setIncoming(Lists.newArrayList("ExclusiveGateway_0yq2l0s"));
+            SequenceFlow_15rfdft.setOutgoing(Lists.newArrayList());
+            Map<String, Object> properties = new HashMap<>();
+            properties.put(Constants.ELEMENT_PROPERTIES.CONDITION, "orderId.equals(\"123\")");
+            SequenceFlow_15rfdft.setProperties(properties);
+
+            FlowElement SequenceFlow_1lc9xoo = new FlowElement();
+            SequenceFlow_1lc9xoo.setKey("SequenceFlow_1lc9xoo");
+            SequenceFlow_1lc9xoo.setType(FlowElementType.SEQUENCE_FLOW);
+            SequenceFlow_1lc9xoo.setIncoming(Lists.newArrayList("ExclusiveGateway_0yq2l0s"));
+            SequenceFlow_1lc9xoo.setOutgoing(Lists.newArrayList());
+            Map<String, Object> _properties = new HashMap<>();
+            _properties.put(Constants.ELEMENT_PROPERTIES.CONDITION, "orderId.equals(\"456\")");
+            SequenceFlow_1lc9xoo.setProperties(_properties);
+
+            Map<String, FlowElement> flowElementMap = new HashMap<>();
+            flowElementMap.put("ExclusiveGateway_0yq2l0s", currentFlowElement);
+            flowElementMap.put("SequenceFlow_15rfdft", SequenceFlow_15rfdft);
+            flowElementMap.put("SequenceFlow_1lc9xoo", SequenceFlow_1lc9xoo);
+
+            Map<String, InstanceData> instanceDataMap = new HashMap<>();
+            instanceDataMap.put("notExist", new InstanceData("orderId", "string", "123456"));
+
+            FlowModelUtil.calculateNextNode(currentFlowElement, flowElementMap, instanceDataMap);
+        } catch (ProcessException e) {
+            e.printStackTrace();
+            // No such property: orderId for class: Script1
+            Assert.assertTrue(e.getErrNo() == ErrorEnum.GROOVY_CALCULATE_FAILED.getErrNo());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
