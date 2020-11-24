a566de859977d51d1cd690e6b774995d590971b3
danniewangdan <danniewangdan@didglobal.com>
2020-11-24 09:54:11 +0800

    add FlowDeploymentDAOTest

---
+++ turbo-engine/src/test/java/com/xiaoju/uemc/turbo/engine/dao/FlowDeploymentDAOTest.java
@@ -0,0 +1,52 @@
+package com.xiaoju.uemc.turbo.engine.dao;
+
+import com.xiaoju.uemc.turbo.engine.entity.FlowDefinitionPO;
+import com.xiaoju.uemc.turbo.engine.entity.FlowDeploymentPO;
+import com.xiaoju.uemc.turbo.engine.runner.BaseTest;
+import com.xiaoju.uemc.turbo.engine.util.EntityBuilder;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.*;
+
+public class FlowDeploymentDAOTest extends BaseTest {
+
+    @Resource
+    FlowDeploymentDAO flowDeploymentDAO;
+
+    @Test
+    public void insert() {
+        FlowDeploymentPO flowDeploymentPO = EntityBuilder.buildFlowDeploymentPO();
+        int result = flowDeploymentDAO.insert(flowDeploymentPO);
+        Assert.assertTrue(result == 1);
+    }
+
+    @Test
+    public void selectByDeployId() {
+        FlowDeploymentPO flowDeploymentPO = EntityBuilder.buildFlowDeploymentPO();
+        flowDeploymentDAO.insert(flowDeploymentPO);
+        String flowDeployId = flowDeploymentPO.getFlowDeployId();
+        flowDeploymentPO = flowDeploymentDAO.selectByDeployId(flowDeployId);
+        Assert.assertTrue(flowDeployId.equals(flowDeploymentPO.getFlowDeployId()));
+
+
+    }
+
+    @Test
+    public void selectRecentByFlowModuleId() {
+        FlowDeploymentPO flowDeploymentPO = EntityBuilder.buildFlowDeploymentPO();
+        flowDeploymentDAO.insert(flowDeploymentPO);
+        FlowDeploymentPO flowDeploymentPONew = EntityBuilder.buildFlowDeploymentPO();
+        String flowModuleId1 = flowDeploymentPO.getFlowModuleId();
+        flowDeploymentPONew.setFlowDeployId("newtest_id");
+        flowDeploymentPONew.setFlowModuleId(flowModuleId1);
+        flowDeploymentDAO.insert(flowDeploymentPONew);
+        FlowDeploymentPO flowDeploymentPORes = flowDeploymentDAO.selectRecentByFlowModuleId(flowModuleId1);
+        Assert.assertTrue(flowDeploymentPONew.getFlowDeployId().equals(flowDeploymentPORes.getFlowDeployId()));
+
+
+
+    }
+}
