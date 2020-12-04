a550d4b73e8bbe4f71d83f32c1dd07215181180e
zuokai <zuokai@didiglobal.com>
2020-12-04 16:32:29 +0800

    update unit test

--- turbo-engine/src/test/java/com/xiaoju/uemc/turbo/engine/runner/BaseTest.java
+++ turbo-engine/src/test/java/com/xiaoju/uemc/turbo/engine/runner/BaseTest.java
@@ -1,8 +1,8 @@
 package com.xiaoju.uemc.turbo.engine.runner;
 
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
