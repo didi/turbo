0461c0987f26eea51a289db7143c2314eb551818
jameszhangxiao123 <15650716091@163.com>
2022-04-21 11:03:44 +0800

    test

--- demo/src/main/java/com/didiglobal/turbo/demo/DemoApplication.java
+++ demo/src/main/java/com/didiglobal/turbo/demo/DemoApplication.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
 @MapperScan(basePackages = {"com.didiglobal.turbo.engine.dao"})
 public class DemoApplication {
     private static final Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
