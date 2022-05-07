d82938d49d16dfaf09d3bce4c738484147384ff3
jameszhangxiao123 <15650716091@163.com>
2022-05-07 18:03:42 +0800

    补充接口测试代码

---
+++ demo/src/main/test/java/com/didiglobal/turbo/demo/logicflow/FlowControllerTest.java
@@ -0,0 +1,166 @@
+package com.didiglobal.turbo.demo.logicflow;
+
+import com.alibaba.fastjson.JSON;
+import com.didiglobal.turbo.demo.DemoApplication;
+import com.didiglobal.turbo.demo.pojo.request.CreateFlowRequest;
+import com.didiglobal.turbo.demo.pojo.request.DeployFlowRequest;
+import com.didiglobal.turbo.demo.pojo.request.GetFlowModuleListRequest;
+import com.didiglobal.turbo.demo.pojo.request.GetFlowModuleRequest;
+import com.didiglobal.turbo.demo.pojo.request.UpdateFlowRequest;
+import com.didiglobal.turbo.demo.pojo.response.BaseResponse;
+import com.didiglobal.turbo.demo.pojo.response.CreateFlowResponse;
+import com.didiglobal.turbo.demo.pojo.response.DeployFlowResponse;
+import com.didiglobal.turbo.demo.pojo.response.FlowModuleListResponse;
+import com.didiglobal.turbo.demo.pojo.response.GetFlowModuleResponse;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ *
+ * demo页面中没有呈现但接口中支持的字段（例如租户标识），使用方可以根据自己业务需求酌情添加
+ * @Author: james zhangxiao
+ * @Date: 5/7/22
+ * @Description: 接口测试类
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
+public class FlowControllerTest {
+
+    @Resource
+    private FlowController flowController;
+
+    private String flowModuleId = null;
+
+    private String flowDeployId = null;
+
+    @Test
+    public void run() {
+        createFlow();
+        saveFlowModel();
+        deployFlow();
+        queryFlow();
+        queryFlowList();
+    }
+
+    /**
+     * 创建流程接口
+     */
+    @Test
+    public void createFlow() {
+        CreateFlowRequest createFlowRequest = new CreateFlowRequest();
+        createFlowRequest.setFlowName("测试流程"); // 模型名称  非必需
+        createFlowRequest.setFlowKey("testFlowKey");  //流程业务标识 非必需
+        createFlowRequest.setTenant("didi");// 租户标识 必需
+        createFlowRequest.setCaller("testCaller");// 使用方标识 必需
+        createFlowRequest.setOperator("didiOperator");// 操作人 选填
+        createFlowRequest.setRemark("备注test");  //备注  选填
+        BaseResponse<CreateFlowResponse> res = flowController.createFlow(createFlowRequest);
+        if (res.getErrCode() == 1000) {
+            flowModuleId = res.getData().getFlowModuleId();//模型唯一标识  必需
+            String str = "createFlow 处理成功 flowModuleId:%s";
+            System.out.println(String.format(str,flowModuleId));
+        } else {
+            throw new RuntimeException("createFlow error");
+        }
+    }
+
+    /**
+     * 保存流程模型接口
+     */
+    @Test
+    public void saveFlowModel() {
+        String flowModel = "{\"flowElementList\":[{\"incoming\":[],\"outgoing\":[\"Flow_3599vu7\"],\"dockers\":[],\"type\":2,\"properties\":{\"name\":\"开始\",\"x\":310,\"y\":200,\"text\":{\"x\":310,\"y\":240,\"value\":\"开始\"}},\"key\":\"Event_0vbtunu\"},{\"incoming\":[\"Flow_3599vu7\"],\"outgoing\":[\"Flow_1f2ei89\"],\"dockers\":[],\"type\":4,\"properties\":{\"name\":\"\",\"x\":520,\"y\":200,\"text\":\"\"},\"key\":\"Activity_0ivtksn\"},{\"incoming\":[\"Flow_1f2ei89\"],\"outgoing\":[\"Flow_1rkk099\"],\"dockers\":[],\"type\":6,\"properties\":{\"name\":\"\",\"x\":730,\"y\":200,\"text\":\"\"},\"key\":\"Gateway_2qj55i1\"},{\"incoming\":[\"Flow_1rkk099\"],\"outgoing\":[],\"dockers\":[],\"type\":3,\"properties\":{\"name\":\"结束\",\"x\":950,\"y\":200,\"text\":{\"x\":950,\"y\":240,\"value\":\"结束\"}},\"key\":\"Event_03pjf39\"},{\"incoming\":[\"Event_0vbtunu\"],\"outgoing\":[\"Activity_0ivtksn\"],\"type\":1,\"dockers\":[],\"properties\":{\"name\":\"\",\"text\":\"\",\"startPoint\":\"{\\\"x\\\":328,\\\"y\\\":200}\",\"endPoint\":\"{\\\"x\\\":470,\\\"y\\\":200}\",\"pointsList\":\"\\\"\\\"\"},\"key\":\"Flow_3599vu7\"},{\"incoming\":[\"Activity_0ivtksn\"],\"outgoing\":[\"Gateway_2qj55i1\"],\"type\":1,\"dockers\":[],\"properties\":{\"name\":\"\",\"text\":\"\",\"startPoint\":\"{\\\"x\\\":570,\\\"y\\\":200}\",\"endPoint\":\"{\\\"x\\\":705,\\\"y\\\":200}\",\"pointsList\":\"\\\"\\\"\"},\"key\":\"Flow_1f2ei89\"},{\"incoming\":[\"Gateway_2qj55i1\"],\"outgoing\":[\"Event_03pjf39\"],\"type\":1,\"dockers\":[],\"properties\":{\"conditionsequenceflow\":\"a==1\",\"name\":\"\",\"text\":\"\",\"startPoint\":\"{\\\"x\\\":755,\\\"y\\\":200}\",\"endPoint\":\"{\\\"x\\\":932,\\\"y\\\":200}\",\"pointsList\":\"\\\"\\\"\"},\"key\":\"Flow_1rkk099\"}]}";
+        UpdateFlowRequest updateFlowRequest = new UpdateFlowRequest();
+        updateFlowRequest.setFlowModuleId(flowModuleId); //模型唯一标识  必需 （使用createFlow中返回的flowModuleId）
+        updateFlowRequest.setFlowModel(flowModel);//模型内容 必需
+        updateFlowRequest.setCaller("testCaller");// 使用方标识  必需
+        updateFlowRequest.setTenant("didi");// 租户标识 必需
+        updateFlowRequest.setFlowKey("testFlowKey");//流程业务标识 选填
+        updateFlowRequest.setFlowName("测试流程"); // 模型名称  选填
+        updateFlowRequest.setRemark("备注test");//备注  选填
+        updateFlowRequest.setOperator("didiOperator");// 操作人 选填
+        BaseResponse<String> res = flowController.saveFlowModel(updateFlowRequest);
+        if (res.getErrCode() == 1000) {
+            System.out.println("saveFlowModel 处理成功");
+        } else {
+            throw new RuntimeException("saveFlowModel error");
+        }
+
+    }
+
+
+    /**
+     * 发布流程接口
+     */
+    @Test
+    public void deployFlow() {
+        DeployFlowRequest deployFlowRequest = new DeployFlowRequest();
+        deployFlowRequest.setFlowModuleId(flowModuleId);//模型唯一标识 必需
+        deployFlowRequest.setTenant("didi"); //租户标识   必需
+        deployFlowRequest.setCaller("testCaller");// 使用方标识  必需
+        deployFlowRequest.setOperator("didiOperator"); // 操作人 选填
+        BaseResponse<DeployFlowResponse> res = flowController.deployFlow(deployFlowRequest);
+        if (res.getErrCode() == 1000) {
+            String flowModuleId = res.getData().getFlowModuleId();//模型唯一标识  必需
+                   flowDeployId = res.getData().getFlowDeployId();//模型一次部署唯一标识  必需
+            String str = "deployFlow 处理成功 flowModuleId:%s flowDeployId：%s";
+            System.out.println(String.format(str,flowModuleId,flowDeployId));
+        } else {
+            throw new RuntimeException("deployFlow error");
+        }
+    }
+
+    /**
+     * 查询单个流程接口
+     */
+    @Test
+    public void queryFlow() {
+        GetFlowModuleRequest getFlowModuleRequest = new GetFlowModuleRequest();
+        getFlowModuleRequest.setFlowModuleId(flowModuleId);//模型唯一标识 两个参数必须传入一个
+        getFlowModuleRequest.setFlowDeployId(flowDeployId);//模型一次部署唯一标识 两个参数必须传入一个
+        BaseResponse<GetFlowModuleResponse> res = flowController.queryFlow(getFlowModuleRequest);
+        if (res.getErrCode() == 1000) {
+            GetFlowModuleResponse getFlowModuleResponse = res.getData();
+            getFlowModuleResponse.getFlowModel();//模型内容 必需
+            getFlowModuleResponse.getFlowModuleId();////模型唯一标识 必需
+            getFlowModuleResponse.getCaller();// 使用方标识  必需
+            getFlowModuleResponse.getTenant();//租户标识   必需
+            getFlowModuleResponse.getFlowName();// 模型名称  非必需
+            getFlowModuleResponse.getFlowKey();//流程业务标识  非必需
+            getFlowModuleResponse.getStatus();//状态码  非必需
+            getFlowModuleResponse.getOperator();// 操作人  非必需
+            getFlowModuleResponse.getRemark();//备注   非必需
+            getFlowModuleResponse.getModifyTime();// 必需
+            System.out.println("queryFlow 处理成功 ");
+        } else {
+            throw new RuntimeException("queryFlow error");
+        }
+    }
+
+    /**
+     * 查询流程列表接口
+     */
+    @Test
+    public void queryFlowList() {
+        GetFlowModuleListRequest getFlowModuleListRequest = new GetFlowModuleListRequest();
+        getFlowModuleListRequest.setFlowName("测试流程");  // 模型名称  选填
+        getFlowModuleListRequest.setFlowModuleId(null);//模型唯一标识  选填
+        getFlowModuleListRequest.setFlowDeployId(null);//模型一次部署唯一标识 选填
+        getFlowModuleListRequest.setCurrent(1);//当前页  选填 默认为1
+        getFlowModuleListRequest.setSize(10);//每页条数   选填 默认为10
+        BaseResponse<FlowModuleListResponse> res = flowController.queryFlowList(getFlowModuleListRequest);
+        if (res.getErrCode() == 1000) {
+            String str = "queryFlowList 处理成功   总条数:%s  当前页：%s 每页条数：%s";
+            str= String.format(str, res.getData().getTotal(),res.getData().getCurrent(),res.getData().getSize());
+            System.out.println(str);
+        } else {
+            throw new RuntimeException("queryFlowList error");
+        }
+    }
+
+}
--- docs/LogicFlowGuide.md
+++ docs/LogicFlowGuide.md
@@ -3,8 +3,10 @@
 LogicFlow 与 Turbo接口配合使用，用于流程配置及管理，根据前端配置流程图生成固定格式数据，这种格式可以被Turbo执行侧识别并被驱动执行
 
 
-## 2 代码样例
-## 2.1 使用LogicFlow
+## 2 时序图
+![在这里插入图片描述](http://img-ys011.didistatic.com/static/didi_opensource/do1_n9zjMcJLM5XPxXv8Hu69?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9saWRvbmcxNjY1LmJsb2cuY3Nkbi5uZXQ=,size_16,color_FFFFFF,t_70)
+## 3 代码样例
+## 3.1 使用LogicFlow
 由于Logicflow默认的数据格式和Turbo默认的数据格式存在一定差异。所以我们需要使用TurboAdapter来实现将Logicflow的数据格式与Turbo数据格式的相互转换。TurboAdapter已经被封装为一个插件，所以直接当做插件使用即可
 
 ```
@@ -26,11 +28,11 @@ this.lf = new LogicFlow({
 如上源码所示：TurboAdapter默认将Turbo数据转换为bpmn节点，在实际项目中，可以完全自定义节点，而不是使用bpmn节点。具体用法请参考LogicFlow自定义节点(http://logic-flow.org/guide/basic/node.html)
 插件代码以及文档地址：https://github.com/Logic-Flow/turbo-client
 
-## 2.2 后端接口
+## 3.2 后端接口
 后端提供样例接口包含创建流程接口，保存流程模型接口，发布流程接口，查询单个流程接口，查询流程列表接口，使用方可以根据自身业务特点合理编排使用
-代码参考com.didiglobal.turbo.demo.logicflow.FlowController
+代码参考com.didiglobal.turbo.demo.logicflow.FlowController 后端接口测试代码com.didiglobal.turbo.demo.logicflow.FlowControllerTest
 
-## 2.3 服务启动
+## 3.3 服务启动
 
 a: 后端服务打包并启动
 
@@ -48,20 +50,20 @@ d: 最后根据日志中提示地址访问即可
 
 
 
-## 3 配置流程
+## 4 配置流程
 整体配置流程为  新建流程---> 编辑流程图---> 保存流程信息--->发布流程
 
-## 3.1 新建流程
+## 4.1 新建流程
 ![在这里插入图片描述](http://img-ys011.didistatic.com/static/didi_opensource/do1_EbjKEbWviwQsVceVatrP?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9saWRvbmcxNjY1LmJsb2cuY3Nkbi5uZXQ=,size_16,color_FFFFFF,t_70)
 
-## 3.2 编辑流程图
+## 4.2 编辑流程图
 ![在这里插入图片描述](http://img-ys011.didistatic.com/static/didi_opensource/do1_DCMEWFfvHV8rX2mKP8mV?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9saWRvbmcxNjY1LmJsb2cuY3Nkbi5uZXQ=,size_16,color_FFFFFF,t_70)
 如果流程图中用到网关节点，则需要根据业务需要添加groovy表达式来决定流程走向
 
-## 3.3 保存流程信息
+## 4.3 保存流程信息
 ![在这里插入图片描述](http://img-ys011.didistatic.com/static/didi_opensource/do1_W25efwUYGlFRwEJ6EQdb?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9saWRvbmcxNjY1LmJsb2cuY3Nkbi5uZXQ=,size_16,color_FFFFFF,t_70)
 
-## 3.4 发布流程
+## 4.4 发布流程
 ![在这里插入图片描述](http://img-ys011.didistatic.com/static/didi_opensource/do1_pgrJUKQu93n4MjVjZMT1?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9saWRvbmcxNjY1LmJsb2cuY3Nkbi5uZXQ=,size_16,color_FFFFFF,t_70)
 
 
