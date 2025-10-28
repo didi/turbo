4bbdee534b727755bc0d0da6b74c49f68fc132c9
zesenwu <zesenwu@didiglobal.com>
2025-10-28 17:55:28 +0800

    feature(turbo): JDK21 升级后，对于新方法校验更加严格，因此使用 @Override 去返回

--- engine/src/main/java/com/didiglobal/turbo/engine/bo/NodeInstance.java
+++ engine/src/main/java/com/didiglobal/turbo/engine/bo/NodeInstance.java
@@ -16,6 +16,16 @@ public class NodeInstance extends ElementInstance {
     private Date modifyTime;
     private Map<String, Object> properties = new HashMap<>();
 
+    @Override
+    public String getNodeInstanceId() {
+        return nodeInstanceId;
+    }
+
+    @Override
+    public void setNodeInstanceId(String nodeInstanceId) {
+        this.nodeInstanceId = nodeInstanceId;
+    }
+
     public Date getCreateTime() {
         return createTime;
     }
